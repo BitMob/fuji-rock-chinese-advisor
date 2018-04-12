01dfe57118664193276333e42f08c3967251c104
Ed Lee <jonexiu@gmail.com>
2018-04-12 18:50:52 +0800

    move to another repo

--- client/components/App/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-import './styled'
-
-const App = ({ children }) => <div>{children}</div>
-
-export default App
--- client/components/Button/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { Button as Btn } from './styled'
-
-const Button = ({ text, onClick }) => <Btn onClick={onClick}>{text}</Btn>
-
-export default Button
